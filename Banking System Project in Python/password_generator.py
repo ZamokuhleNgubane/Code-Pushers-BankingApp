f03297c9a7617d6c11b2c0e7b7cd74f396b79a5a
DanielleJadine <Danielle.Engelbrecht@capaciti.org>
2024-05-15 10:50:09 +0200

    password_generator

---
+++ Banking System Project in Python/password_generator.py
@@ -0,0 +1,13 @@
+import random
+import string
+
+def generate_password(length=12):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    return ''.join(random.choice(characters) for _ in range(length))
+
+def main():
+    password = generate_password()
+    print("Random Password:", password)
+
+if __name__ == "__main__":
+    main()
